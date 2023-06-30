2c5c9ad6f4d7b4460b95b03d679862b8c510922e
Varaprasadjagu <83534118+Varaprasadjagu@users.noreply.github.com>
2023-06-30 16:30:23 +0530

    Update ashish.c

--- ashish.c
+++ ashish.c
@@ -3,7 +3,5 @@
 int main() {
 	char* string = "Ashish aman."; // Your name here. ^_^
 	char* string = "Ashish aman."; // Your name here. ^_^
-  char* string = "Sunny Arora."; // Your name here. ^_^
-	printf("%s\n", string);
-	return 0;
+
 }
